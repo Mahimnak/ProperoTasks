ff4cf039072ceb21e9882e23fc4d5e43d14409d3
Mahimna Kanoongo <mahimna77@gmail.com>
2024-02-15 17:06:06 +0530

    Task 7 starting BotsDNA BackgroundVerification

---
+++ Task 3 BotsDNA/7-BackgroundVerification/tasks.py
@@ -0,0 +1,20 @@
+from robocorp.tasks import task
+from robocorp import browser
+from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
+import openpyxl
+from openpyxl import Workbook
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.support.relative_locator import locate_with
+from selenium.webdriver.support.select import Select
+
+@task
+def background_check():
+    """Background verification and handling missing documents"""
