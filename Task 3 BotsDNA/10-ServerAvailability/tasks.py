8f9ed2dd4b7156394106ce362a98ee0dd4847316
Mahimna Kanoongo <mahimna77@gmail.com>
2024-02-20 17:32:53 +0530

    task 10 completion ServerAvailability BotsDNA

--- Task 3 BotsDNA/10-ServerAvailability/tasks.py
+++ Task 3 BotsDNA/10-ServerAvailability/tasks.py
@@ -17,7 +17,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import zipfile
 from selenium.webdriver.support.select import Select
-from RPA.Email.ImapSmtp import ImapSmtpClient
+
 @task
 def ServerAvailability():
-    
+    """Check whether a server is available or not"""
+    url="https://botsdna.com/ServerAvailability/"
+    driver = webdriver.Chrome()
+    for i in range(2,37):
+        driver.get(url)
+        workbook = openpyxl.load_workbook('input.xlsx')
+        sheet = workbook.active 
+        user_id = sheet.cell(row=i, column=1).value
+        password = sheet.cell(row=i, column=2).value
+        user_input = driver.find_element(By.ID,"username")
+        user_input.send_keys(user_id)
+        pass_input = driver.find_element(By.ID, "password")
+        pass_input.send_keys(password)
+        server = driver.find_element(By.XPATH,f"//*[@id='name']/option[{i}]")
+        server.click()
+        submit = driver.find_element(By.XPATH,"//table/tbody/tr[4]/td[2]/input")
+        submit.click()
+        status = driver.find_element(By.XPATH,"//*[@id='status']").text
+        status = status.splitlines()[1]
+        sheet.cell(row=i, column=5, value = status)
+        workbook.save("input.xlsx")
