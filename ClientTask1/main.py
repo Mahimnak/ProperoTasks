af55dc01fde16c6d642ccd9e54e300f01b5d8e29
Mahimna Kanoongo <mahimna77@gmail.com>
2024-05-06 13:39:33 +0530

    Final changes to the file

--- ClientTask1/main.py
+++ ClientTask1/main.py
@@ -1,31 +1,53 @@
 import openpyxl
 import csv
-import xlrd
-from xls2xlsx import XLS2XLSX
+# from xls2xlsx import XLS2XLSX
 import pandas as pd
 import re
+import datetime
 
 class Order:
     def __init__(self):
         pass
-
+    
+    def num_to_char(self, n):
+        return chr(64 + n)
+    
     def create_output_file(self, filename="Output.xlsx"):
         """Create a new output file with a default name 'Output.xlsx'."""
         try:
-            workbook = openpyxl.Workbook()
-            sheet = workbook.active
+            # Load the template workbook
+            source_workbook = openpyxl.load_workbook('output\Template.xlsx')
 
-            workbook.save(f'output/{filename}')
+            # Get the active sheet
+            active_sheet = source_workbook.active
+
+            # Create a new workbook
+            new_workbook = openpyxl.Workbook()
+
+            # Add a new sheet to the new workbook with the same name as the active sheet
+            new_sheet = new_workbook.active
+            new_sheet.title = active_sheet.title
+
+            # Copy the cells from the active sheet to the new sheet
+            for row in active_sheet.iter_rows():
+                new_row = []
+                for cell in row:
+                    new_row.append(cell.value)
+                new_sheet.append(new_row)
+
+            # Save the new workbook to a file
+            new_workbook.save(f'output\{filename}')
         except Exception as e:
             print(f"Error creating output file: {e}")
 
-    def convert_to_xlsx_file(self, filename="Consolidated Order Statement 10022024.xls"):
-        """Convert a .xls file to .xlsx file for easier formatting."""
-        try:
-            x2x = XLS2XLSX(f"input/{filename}")
-            x2x.to_xlsx(f"Order_Statement.xlsx")
-        except Exception as e:
-            print(f"Error converting to xlsx file: {e}")
+    # def convert_to_xlsx_file(self, filename="Consolidated Order Statement 10022024.xls"):
+    #     """Convert a .xls file to .xlsx file for easier formatting."""
+    #     try:
+    #         x2x = XLS2XLSX(f"input/{filename}")
+    #         x2x.to_xlsx(f"Order_Statement.xlsx")
+    #     except Exception as e:
+    #         print(f"Error converting to xlsx file: {e}")
+
 
     def csv_to_xlsx_file(self, filename = "input\input.csv"):
         """Convert a .csv file to .xlsx file for easier formatting."""
@@ -69,16 +91,16 @@ class Order:
                     wb = openpyxl.load_workbook(f'input\Input.xlsx')
                     ws = wb.active
                 except Exception as e:
-                    raise FileNotFoundError("Unable to locate the Input file.")
-
-            # Load the input file
-            
+                    raise FileNotFoundError("Unable to locate the Input file.")            
 
             # Store data in lists
             col_a = [cell.value for cell in ws['A'][1:] if cell.value is not None]
             col_q = [cell.value for cell in ws['Q'][1:] if cell.value is not None]
             col_r = [cell.value for cell in ws['R'][1:] if cell.value is not None]
             col_y = [cell.value for cell in ws['Y'][1:] if cell.value is not None]
+            
+            col_y = list(set(col_y))
+            
 
             number_of_clients = list(set(col_a))  # remove duplicates from list
 
@@ -107,15 +129,22 @@ class Order:
             unit = ""
             wb = openpyxl.load_workbook(f'output/Output.xlsx')
             ws = wb.active
+            current_date = datetime.date.today()
+            # Format the current date in the DD/MM/YYYY format
+            formatted_date = current_date.strftime("%d/%m/%Y")
+            ws.cell(row=3, column=1, value=f"Date : {formatted_date}")
 
-            ws.cell(row=1, column=1, value="Sr. No.")
-            ws.cell(row=1, column=2, value="Product")
-            ws.cell(row=1, column=3, value="UOM")
-            ws.cell(row=1, column=4, value="Qty.")
+            ws.cell(row=5, column=1, value="Sr. No.")
+            ws.cell(row=5, column=2, value="Product")
+            ws.cell(row=5, column=3, value="UOM")
+            ws.cell(row=5, column=4, value="Qty.")
 
             for i in range(len(clients)):
-                ws.cell(row=1, column=5+i, value=clients[i])
-
+                ws.cell(row=5, column=5+i, value=clients[i])
+            # cell_value = len(clients)-1+5
+            # print(self.num_to_char(cell_value))
+            # ws.merge_cells(f'A1:{self.num_to_char(26)}1')
+            
             for i in range(len(vegetables)):
                 if "Gram" in uom[vegetables[i]] or "gram" in uom[vegetables[i]] or "Kg" in uom[vegetables[i]] or "kg" in uom[vegetables[i]]:
                     unit = "Kg"
@@ -141,12 +170,21 @@ class Order:
 
             for row in ws1.iter_rows(values_only=True):
                 if row[0] in clients:
-                    for col_idx, cell_value in enumerate(ws2[1], start=1):  # Iterate over cells in the first row
+                    for col_idx, cell_value in enumerate(ws2[5], start=1):  # Iterate over cells in the first row
                         if cell_value.value == row[0]:  # Check if the cell value matches the client identifier
+                            if row[24]!=None:
+                                substrings = row[24].split()
+                                # Extract the first letter of each substring
+                                initials = [substring[0] for substring in substrings]
+                                # Join the initials into a single string
+                                initials_string = ''.join(initials)
+                                ws2.cell(row = 4, column = col_idx, value = initials_string.upper())
                             col_counter = col_idx  # Update the column counter with the column index
                             break
                     for row_value in ws2.iter_rows(values_only=True):
-                        if any(cell == row[17].split("|")[0].strip() for cell in row_value):
+                        if row_counter<=5:
+                            row_counter += 1
+                        elif any(cell == row[17].split("|")[0].strip() for cell in row_value):
                             row_of_veg = row_counter
                             break
                         else:
@@ -165,7 +203,7 @@ class Order:
             total = 0
 
             for row in ws2.iter_rows(values_only=True):
-                if row_counter==1:
+                if row_counter<=5:
                     row_counter+=1
                     pass
                 else:
@@ -180,6 +218,7 @@ class Order:
         except Exception as e:
             print(f"Error adding data: {e}")
 
+    #Only for testing purposes does not add any meaning to the application.
     def testing(self):
         try:
             wb1 = openpyxl.load_workbook('Order_Statement.xlsx')
@@ -190,5 +229,3 @@ class Order:
                 break
         except Exception as e:
             print(f"Error testing: {e}")
-
-# Create a new order object and call its method to retrieve data from Excel file
