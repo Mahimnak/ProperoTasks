d4c1d554de3180d16d2a300a42883fe8c148f258
Mahimna Kanoongo <mahimna77@gmail.com>
2024-05-15 17:59:54 +0530

    Changes - Bold, Borders, Font Size. Simple Python file instead of robot

--- ClientTask1/main.py
+++ ClientTask1/main.py
@@ -1,6 +1,6 @@
 import openpyxl
+from openpyxl.styles import Font, NamedStyle, Border, Side
 import csv
-# from xls2xlsx import XLS2XLSX
 import pandas as pd
 import re
 import datetime
@@ -134,23 +134,62 @@ class Order:
             formatted_date = current_date.strftime("%d/%m/%Y")
             ws.cell(row=3, column=1, value=f"Date : {formatted_date}")
 
-            ws.cell(row=5, column=1, value="Sr. No.")
-            ws.cell(row=5, column=2, value="Product")
-            ws.cell(row=5, column=3, value="UOM")
-            ws.cell(row=5, column=4, value="Qty.")
+            
+
+            named_style = NamedStyle(name="bordered")
+            named_style.border = Border(left=Side(style='thin'), 
+                                        right=Side(style='thin'), 
+                                        top=Side(style='thin'), 
+                                        bottom=Side(style='thin'))
+
+            ws["A1"].font = Font(size=18, bold=True)
+            ws["A2"].font = Font(size=18, bold=True)
+            ws["A3"].font = Font(size=18, bold=True)
+
+            ws.cell(row=5, column=1, value="Sr. No.").style = named_style
+            ws.cell(row=5, column=2, value="Product").style = named_style
+            ws.cell(row=5, column=3, value="UOM").style = named_style
+            ws.cell(row=5, column=4, value="Qty.").style = named_style
+
+            ws["A5"].font = Font(size=14, bold=True)
+            ws["B5"].font = Font(size=14, bold=True)
+            ws["C5"].font = Font(size=14, bold=True)
+            ws["D5"].font = Font(size=14, bold=True)
 
             for i in range(len(clients)):
-                ws.cell(row=5, column=5+i, value=clients[i])
+                ws.cell(row=5, column=5+i, value=clients[i]).style = named_style
+                ws.cell(row=5, column=5+i).font = Font(size = 14, bold=True)
             # cell_value = len(clients)-1+5
             # print(self.num_to_char(cell_value))
             # ws.merge_cells(f'A1:{self.num_to_char(26)}1')
             
+            
+
             for i in range(len(vegetables)):
                 if "Gram" in uom[vegetables[i]] or "gram" in uom[vegetables[i]] or "Kg" in uom[vegetables[i]] or "kg" in uom[vegetables[i]]:
                     unit = "Kg"
                 elif "Pc" in uom[vegetables[i]] or "pc" in uom[vegetables[i]]:
                     unit = "Pc"
                 ws.append([i+1, vegetables[i], unit])
+
+                ws.cell(row = i+6, column = 1).style = named_style
+                ws.cell(row = i+6, column = 2).style = named_style
+                ws.cell(row = i+6, column = 3).style = named_style
+                ws.cell(row = i+6, column = 1).font = Font(size = 14)
+                ws.cell(row = i+6, column = 2).font = Font(size = 14)
+                ws.cell(row = i+6, column = 3).font = Font(size = 14)
+                
+
+            ctr = 1 
+            max_col = ws.max_column
+            max_rows = ws.max_row
+            for j in range(1,max_rows+1):
+                if j<=5:
+                    ctr += 1
+                elif j>5:
+                    for i in range(5,max_col+1):
+                        ws.cell(row = ctr, column = i, value = None).style = named_style
+                    ctr+=1
 
             wb.save(f'output/Output.xlsx')
         except Exception as e:
@@ -162,6 +201,12 @@ class Order:
             col_counter = 1
             row_counter = 1
 
+            named_style = NamedStyle(name="new_borders")
+            named_style.border = Border(left=Side(style='thin'), 
+                                        right=Side(style='thin'), 
+                                        top=Side(style='thin'), 
+                                        bottom=Side(style='thin'))
+            
             wb1 = openpyxl.load_workbook('input\Input.xlsx')
             ws1 = wb1.active
 
@@ -178,9 +223,12 @@ class Order:
                                 initials = [substring[0] for substring in substrings]
                                 # Join the initials into a single string
                                 initials_string = ''.join(initials)
-                                ws2.cell(row = 4, column = col_idx, value = initials_string.upper())
+                                ws2.cell(row = 4, column = col_idx).style = named_style
+                                ws2.cell(row = 4, column = col_idx, value = initials_string.upper()).font = Font(size = 14, bold=True)
+
                             col_counter = col_idx  # Update the column counter with the column index
                             break
+                    
                     for row_value in ws2.iter_rows(values_only=True):
                         if row_counter<=5:
                             row_counter += 1
@@ -196,7 +244,8 @@ class Order:
                             qty *= row[16]
                         else:
                             qty *= row[16]
-                        ws2.cell(row=row_of_veg, column=col_counter, value=qty)
+                        ws2.cell(row=row_of_veg, column=col_counter, value=qty).font = Font(size = 14)
+
                     wb2.save(f'output/Output.xlsx')
                 row_counter = 1
             row_counter = 1
@@ -210,7 +259,8 @@ class Order:
                     for i in range(4,len(row)):
                         if row[i]  != None:
                             total += float(row[i])
-                    ws2.cell(row=row_counter, column=4, value=total)
+                    ws2.cell(row=row_counter, column=4).style = named_style
+                    ws2.cell(row=row_counter, column=4, value=total).font = Font(size =14)
                     row_counter+=1
                     total = 0
             wb2.save(f'output/Output.xlsx')
@@ -229,3 +279,7 @@ class Order:
                 break
         except Exception as e:
             print(f"Error testing: {e}")
+
+if __name__ == "__main__":
+    order = Order()
+    order.get_data()
--- ClientTask1/tasks.py
+++
@@ -1,6 +0,0 @@
-from robocorp.tasks import task
-from main import Order
-@task
-def task():
-    order = Order()
-    order.get_data()
